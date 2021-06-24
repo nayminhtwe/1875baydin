0c050ece92dbfac1642b976d70f49dc92f71c247
naymin <konayminhtwe@gmail.com>
2021-06-24 12:13:45 +0630

    authenticate combining signin & signup

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -69,6 +69,57 @@ exports.signIn = async (req, res) => {
 
 }
 
+exports.authenticate = async (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).send({
+            message: 'Email and Password Required'
+        })
+    }
+    const invalidMsg = {
+        message: "Invalid Email and Password Combination."
+    }
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            },
+            attributes: ['id', 'email', 'password']
+        })
+        // cannot find user condition
+        if (!user) {
+            try {
+                const user = await User.create(req.body)
+                const token = createAccessToken(user)
+                res.status(201).send({
+                    message: "User created",
+                    access_token: token
+                })
+            } catch (e) {
+                console.error(e);
+                res.status(409).send({
+                    message: "Email already exists."
+                })
+            }
+
+        }
+
+        //check password valid or not
+        const match = await validatePassword(req.body.password, user.password)
+        //password match or not condition
+        if (!match) return res.status(401).send(invalidMsg)
+        //end check
+
+        const token = createAccessToken(user)
+        return res.send({
+            message: 'Login Successful.',
+            access_token: token
+        })
+    } catch (e) {
+        return res.status(500).send('Internal Server Error')
+    }
+
+}
+
 exports.profile = async (req, res) => {
     const subscription = await getSubscription(req.user.id)
     const categorySubscriptions = subscription.CategorySubscriptions
--- src/app/routers/userRouter.js
+++ src/app/routers/userRouter.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
-const {authCheckMiddleware} = require('../../utils/auth')
-const {signIn, signUp, profile, userInfo} = require('../controllers/authController')
+const { authCheckMiddleware } = require('../../utils/auth')
+const { signIn, signUp, profile, authenticate, userInfo } = require('../controllers/authController')
 
 
 router.post('/sign-in', signIn)
 router.post('/sign-up', signUp)
+router.post('/authenticate', authenticate)
 router.post('/user_info', userInfo)
 router.get('/profile', authCheckMiddleware, profile)
 module.exports = router
